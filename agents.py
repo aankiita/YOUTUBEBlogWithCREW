e85d8f38e432be730c75f42e80b216bf73424c60
ANKITA YADAV <145215095+aankiita@users.noreply.github.com>
2026-03-02 01:02:01 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,40 +1,40 @@
-from crewai import Agent, LLM  # <-- Import LLM from crewai
+from crewai import Agent, LLM
 from tools import yt_tool
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-groq_llm = LLM(   #Connects to Groq
+groq_llm = LLM(   
     model="groq/llama-3.3-70b-versatile",
     api_key=os.getenv("GROQ_API_KEY")
 )
 
-# Research Agent
-blog_researcher = Agent(   #We are creating first agent.
+
+blog_researcher = Agent(  
     role="Senior YouTube Content Researcher",
     goal="Fetch transcript from YouTube video and extract key insights about {topic}",
     verbose=True,
-    memory=True,   #Agent remembers previous conversation.
+    memory=True,   
     backstory=(
         "You are an AI expert specializing in AI, ML, Data Science, and GenAI."
         "You analyze video transcripts deeply and extract structured insights."
     ),
     llm=groq_llm,  
-    tools=[yt_tool],  #This allows agent to use your YouTube transcript tool.
-    allow_delegation=True, #Research agent can give work to another agent if needed.
+    tools=[yt_tool], 
+    allow_delegation=True, 
 )
 
-# Writer Agent   # #We are creating second agent.
+
 blog_writer = Agent(
     role="Tech Blog Writer",
     goal="Create an engaging blog post based on transcript insights about {topic}",
     verbose=True,
-    memory=True,   #Agent remembers previous conversation.
+    memory=True,   
     backstory=(
         "You simplify complex AI topics into engaging and easy-to-read blog posts."
     ),
     llm=groq_llm,  
-    tools=[yt_tool], #This allows agent to use your YouTube transcript tool.
-    allow_delegation=False, #Research agent cannot give work to another agent if needed.
+    tools=[yt_tool],
+    allow_delegation=False,
 )
