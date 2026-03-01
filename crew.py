4ba6df5886f1aa6d608de6e5b24d82a5738fd07e
ANKITA YADAV <145215095+aankiita@users.noreply.github.com>
2026-03-02 01:03:27 +0530

    Update crew.py

--- crew.py
+++ crew.py
@@ -1,24 +1,24 @@
-from crewai import Crew, Process  #Crew → Used to create a team of agents  and Process → Decides how tasks run (sequential or parallel)
-from agents import blog_researcher, blog_writer  #Importing the two agents you created earlier.
-from tasks import research_task, write_task  #Importing the two tasks you created.
+from crewai import Crew, Process 
+from agents import blog_researcher, blog_writer  
+from tasks import research_task, write_task  
 
-crew = Crew(  #This creates your AI team.
-    agents=[blog_researcher, blog_writer],  #So your crew has: Researcher ,Writer
-    tasks=[research_task, write_task],  #So crew knows:Step 1 → Research ,Step 2 → Write
-    process=Process.sequential,  #Research → then → Writing
-    memory=True,  #Crew remembers previous results.
-    cache=True,  #If same input is used again,  ->it may reuse previous result instead of running again.
+crew = Crew(  
+    agents=[blog_researcher, blog_writer],  
+    tasks=[research_task, write_task],  
+    process=Process.sequential, 
+    memory=True, 
+    cache=True, 
     max_rpm=100,
-    share_crew=True,  #Allows sharing crew state between tasks.
+    share_crew=True, 
 )
 
-if __name__ == "__main__":  #Only run this code if file is executed directly -->Not when imported somewhere else
+if __name__ == "__main__": 
     result = crew.kickoff(
-        inputs={ #  These values replace:{topic} {video_url} from tools.py,agents.py and task.py
+        inputs={ 
             "topic": "AI vs ML vs DL vs Data Science",
             "video_url": "https://youtu.be/k2P_pHQDlp0?si=b0FOr1iXZ07tmAud"
         }
     )
 
-    print("\n\nFINAL OUTPUT:\n")  #This prints final result in terminal.
+    print("\n\nFINAL OUTPUT:\n") 
     print(result)
